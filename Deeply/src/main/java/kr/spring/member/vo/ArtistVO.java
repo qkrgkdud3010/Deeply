bd1c197269e683780c5899f5587720b25f468b1c
seohk <seohk@bongsu>
2025-01-07 14:30:43 +0900

    artistVO 업로드

---
+++ Deeply/src/main/java/kr/spring/member/vo/ArtistVO.java
@@ -0,0 +1,38 @@
+package kr.spring.member.vo;
+
+import java.sql.Date;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(exclude = {"photo"})
+	public class ArtistVO {
+	    private Long user_num;         // 유저 번호 (PK, FK로 사용될 시퀀스)
+	    @NotBlank(message = "그룹 이름은 필수 입력 값입니다.")
+	    private String group_name;     // 그룹 이름
+	    @NotBlank(message = "이름은 필수 입력 값입니다.")
+	    private String name;  
+
+	    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+	    @Pattern(regexp = "^[A-Za-z0-9!@#$%^&*()_+]{8,20}$", message = "비밀번호는 8~20자의 영문자, 숫자, 특수문자로 구성되어야 합니다.")// 아티스트 이름
+	    private String passwd_hash;    // 비밀번호 해시
+	    // 프로필 사진 이름 지정
+	    @NotBlank(message = "코드는 필수 입력 값입니다.")
+	    private String code;
+	    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+	    @Email(message = "유효한 이메일 형식이 아닙니다.")
+	    private String email;
+	    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+	    private String id;
+	    @NotBlank(message = "비밀번호 확인은 필수 입력 값입니다.")
+	    private String confirmPassword;
+	    
+}
+
