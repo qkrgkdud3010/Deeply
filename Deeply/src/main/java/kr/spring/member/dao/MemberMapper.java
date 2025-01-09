1d5b2e8fafa9a983d0f3cce1ec9079edb045a51a
merrin <khr2937@naver.com>
2025-01-09 23:44:35 +0900

    follow에 필요한 selectMember, selectDMember 수정

--- Deeply/src/main/java/kr/spring/member/dao/MemberMapper.java
+++ Deeply/src/main/java/kr/spring/member/dao/MemberMapper.java
@@ -26,7 +26,10 @@ public interface MemberMapper {
 
 	public MemberVO selectCheckMember(String id);
 	Optional<MemberVO> findByEmail(String email);
+	@Select("SELECT * FROM duser JOIN duser_detail USING (user_num) WHERE user_num=#{user_num}")
 	public MemberVO selectMember(Long mem_num);
+	@Select("SELECT * FROM auser JOIN auser_detail USING (user_num) WHERE user_num=#{user_num}")
+	public MemberVO selectDMember(Long mem_num);
 	void updateMember(MemberVO memberVO);
 	
 	@Select("SELECT d.id FROM duser d JOIN duser_detail dd ON d.user_num = dd.user_num WHERE dd.email = #{email} AND dd.name = #{name}")
