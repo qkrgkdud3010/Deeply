17d583d1a77bee69d72b59c3a04327cc10a6e637
merrin <khr2937@naver.com>
2025-01-16 16:38:53 +0900

    오류 수정

--- Deeply/src/main/java/kr/spring/alarm/service/AlarmServiceImpl.java
+++ Deeply/src/main/java/kr/spring/alarm/service/AlarmServiceImpl.java
@@ -1,11 +1,14 @@
 package kr.spring.alarm.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.spring.alarm.dao.AlarmMapper;
 import kr.spring.alarm.vo.AlarmVO;
 
-
+@Service
+@Transactional
 public class AlarmServiceImpl implements AlarmService{
 
 	@Autowired
